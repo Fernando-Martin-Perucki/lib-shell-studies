656e21501e534c4fa60fd25f6520bb9a621b39e2
fernando-martin-perucki <169342514+Fernando-Martin-Perucki@users.noreply.github.com>
2024-05-27 17:25:34 +0900

    Create arithmetic.sh
    WIP

---
+++ arithmetic.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+# -------------------
+# Arithmetic expressions
+# -------------------
+# +     addition
+# -     substraction
+# *     multiplication
+# /     division
+# **    exponentiation
+# %     modulus
